a3fdc30e6ee79ecd9f221421cc7055ee61c28cf7
uliataschunska <popkina2@gmail.com>
2025-11-10 16:44:15 +0200

    Перше практичне заняття

--- jsAnanatra/script.js
+++ jsAnanatra/script.js
@@ -1,2 +1,19 @@
-const answer = prompt( "are you 18?", " ");
-console.log (answer);
+
+const numberOfFilms =  +prompt ( 'Скільки фільмів ви уже переглянули?', ' ' );
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один із останніх переглянутих фільмів?'),
+      b=prompt ('На скільки оціните його?'),
+      c = prompt ('Один із останніх переглянутих фільмів?'),
+      d = prompt ('На скільки оціните його?');
+
+  personalMovieDb.movies[a]=b;
+  personalMovieDb.movies[c]=d;
+
+console.log( personalMovieDb);
